43fd32acd476eef591218f6eaa38dd73c6510561
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-25 15:57:37 -0700

    Update blanker.js

--- JS/blanker.js
+++ JS/blanker.js
@@ -1,7 +1,8 @@
-var buttonEnabled = getCookie("blankerEnabled")
+var buttonEnabled = getCookie("buttonEnabled")
 
-function openPopupCloaked(url, cloakURL) {
-    const popup = window.open(cloakURL, "_blank");
+function openPopupCloaked(url) {
+
+    const popup = window.open("about:blank", "_blank");
     if (popup) {
         const iframe = popup.document.createElement("iframe");
         iframe.src = url;
@@ -10,8 +11,19 @@ function openPopupCloaked(url, cloakURL) {
         popup.document.body.style.margin = "0";
         popup.document.body.appendChild(iframe);
         
+        const userAgent = navigator.userAgent;
+        let browserImg = "/images/newtab.png";
+        
+        if (userAgent.match(/edg/i)) {
+            browserName = "/images/newtab-edge.png";
+        }
+
+        const favicon = popup.document.getElementById("favicon")
+        favicon.href = browserImg
+
         setTimeout(() => window.close(), 500);
     }
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -23,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (buttonEnabled) {
         button.style.color = trueColor;
         button.textContent = "Blanker On";
+        openPopupCloaked(window.location.href, true);
     } else {
         button.style.color = falseColor;
         button.textContent = "Blanker Off";
@@ -33,14 +46,14 @@ document.addEventListener("DOMContentLoaded", function() {
             buttonEnabled = false;
             button.style.color = falseColor;
             button.textContent = "Blanker Off";
-            setCookie("blankerEnabled", false)
-            openPopupCloaked("https://bendover111222333444.onrender.com", "https://bendover111222333444.onrender.com");
-        } else if {
+            setCookie("buttonEnabled", false)
+            window.open("bendover111222333444.onrender.com", _blank);
+        } else {
             buttonEnabled = true;
             button.style.color = trueColor;
             button.textContent = "Blanker On";
-            setCookie("blankerEnabled", true)
-            openPopupCloaked("https://bendover111222333444.onrender.com", "about:blank");
+            setCookie("buttonEnabled", true)
+            openPopupCloaked(window.location.href);
         }
     });
 
@@ -48,8 +61,20 @@ document.addEventListener("DOMContentLoaded", function() {
         link.addEventListener("click", function(e) {
             if (buttonEnabled) {
                 e.preventDefault();
-                openPopupCloaked(this.href, "about:blank");
+                openPopupCloaked(this.href, false);
             }
         });
     });
 });
+
+const url = c;
+const popup = window.open("about:blank", "_blank");
+if (popup) {
+    const iframe = popup.document.createElement("iframe");
+    iframe.src = url;
+    iframe.style = "position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;";
+    popup.document.title = "Tuffness Overload";
+    popup.document.body.style.margin = "0";
+    popup.document.body.appendChild(iframe);
+    setTimeout(() => window.close(), 500);
+}
