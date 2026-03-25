0c9f0c2182aec222f281c02edd000cc31619b8d5
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-25 16:26:48 -0700

    sd
    asd

--- JS/blanker.js
+++ JS/blanker.js
@@ -4,21 +4,14 @@ function openPopupCloaked(url, cloakedUrl) {
 
     const popup = window.open(cloakedUrl, "_blank");
     if (popup) {
-        const iframe = popup.document.createElement("iframe");
-        iframe.src = url;
-        iframe.style = "position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;";
-        popup.document.title = "New Tab";
-        popup.document.body.style.margin = "0";
-        popup.document.body.appendChild(iframe);
         popup.buttonEnabled = buttonEnabled;
 
         const userAgent = navigator.userAgent;
         let browserImg = "/images/newtab.png";
-        
         if (userAgent.match(/edg/i)) {
             browserImg = "/images/newtab-edge.png";
         }
-
+        
         const doc = popup.document;
         doc.open();
         doc.write(`<!DOCTYPE html><html><head>
@@ -26,6 +19,11 @@ function openPopupCloaked(url, cloakedUrl) {
             <title>New Tab</title>
             </head><body style="margin:0"></body></html>`);
         doc.close();
+
+        const iframe = doc.createElement("iframe");
+        iframe.src = url;
+        iframe.style = "position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;";
+        doc.body.appendChild(iframe);
 
         setTimeout(() => popup.opener && popup.opener.close(), 500);
     }
